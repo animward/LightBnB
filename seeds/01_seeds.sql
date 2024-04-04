4631cd1bd9bc16fd39cf204565418b9028e78ba9
animward <animward@gmail.com>
2024-04-03 17:31:02 -0700

    insert property data

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -6,11 +6,11 @@ VALUES
   ('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 -- insert data into the properties table
-INSERT INTO properties (owner_id, title, description, cost_per_night, number_of_bedrooms, number_of_bathrooms)
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
 VALUES
-  (1, 'Property1', 'Description1', 100, 2, 1),
-  (2, 'Property2', 'Description2', 200, 3, 2),
-  (3, 'Property3', 'Description3', 150, 1, 1);
+  (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 93061, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', 28142, true),
+  (1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 83680, true),
+  (2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', 44583, true);
 
 -- insert data into the reservations table
 INSERT INTO reservations (start_date, end_date, property_id, guest_id, number_of_guests)
