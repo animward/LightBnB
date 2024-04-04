bf72f851bdde22f9c81e7aab7c244f0ec883f683
animward <animward@gmail.com>
2024-04-03 17:37:16 -0700

    added more unique messages

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -8,9 +8,9 @@ VALUES
 -- insert data into the properties table
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
 VALUES
-  (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 93061, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', 28142, true),
-  (1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 83680, true),
-  (2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', 44583, true);
+  (1, 'Speed lamp', 'very nice', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 93061, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', 28142, true),
+  (1, 'Blank corner', 'ooh lah lah', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 83680, true),
+  (2, 'Habit mix', 'it has a chair', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', 44583, true);
 
 -- insert data into the reservations table
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
@@ -21,6 +21,6 @@ VALUES
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES 
-  (3, 2, 1, 3, 'messages'),
-  (2, 2, 2, 4, 'messages'),
-  (3, 1, 3, 4, 'messages');
+  (3, 2, 1, 3, 'my wife likes it very much'),
+  (2, 2, 2, 4, 'me and a buddy had a fight here but the place was very nice'),
+  (3, 1, 3, 4, 'yes');
